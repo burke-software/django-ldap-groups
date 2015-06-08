84ee68ec1aeaa343637abf034387d76fad0258f2
David Burke <david@burkesoftware.com>
2015-06-07 22:07:30 -0400

    Finally fixed annoying setup.py stuff

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
-#!/usr/bin/env python
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='django-ldap-groups-bsc',
-      version='0.1.8.2',
+      version='0.1.8.4',
       author='Peter Herndon',
       author_email='tpherndon@gmail.com',
       url='http://code.google.com/p/django-ldap-groups/',
       description='A Django app for authenticating and authorizing against LDAP',
-      packages=['ldap_groups', 'ldap_groups.accounts'],
-      package_data={'ldap_groups': ['templates/admin/ldap_groups/ldapgroup/*']},
+      packages=find_packages(),
+      include_package_data=True,
       classifiers=[
           'Development Status :: 4 - Beta',
           'Environment :: Web Environment',
